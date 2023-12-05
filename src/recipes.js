0d90ca9e02f6a398b6dc2cee98d24bfc90f8ca34
Cam Johnson <cameron.n.johnson15@gmail.com>
2023-12-05 13:12:38 -0700

    Merge files
    Co-authored-by:  Matthew Shindel : <matthew@shindel.com>
    Co-authored-by:  Ricky Tran : <therichardtran@gmail.com>
    Co-authored-by:  Jeremy Pearson : <jeremy.pearson08@gmail.com>

--- src/recipes.js
+++ src/recipes.js
@@ -8,7 +8,36 @@ function getRecipeByName(recipes, name) {
   );
 }
 
+function getIngredientNames(recipe , ingredientsData) {
+	if ((!recipe) || (!ingredientsData)) {
+		return 'Not a valid recipe';
+	}
+
+	return recipe.ingredients.map(ingredient => ingredient.id).map(id => ingredientsData.find(ingredient => ingredient.id === id)).map(ingredient => ingredient.name);
+}
+
+function getIngredientPriceSum(recipe , ingredientsData) {
+	if ((!recipe) || (!ingredientsData)) {
+		return 'Not a valid recipe';
+	}
+
+	return recipe.ingredients.reduce((acc, cost) => acc + (
+		cost.quantity.amount * ingredientsData.find(ingredient => ingredient.id === cost.id).estimatedCostInCents
+	), 0);
+}
+
+function getRecipeInstructions(recipe) {
+	if (!recipe) {
+		return 'Not a valid recipe';
+	}
+	
+	return recipe.instructions;
+}
+
 export {
   filterByTag,
-  getRecipeByName
+  getRecipeByName,
+  getIngredientNames,
+	getIngredientPriceSum,
+	getRecipeInstructions
 };
--- test/recipes-test.js
+++ test/recipes-test.js
@@ -1,117 +1,213 @@
 import { expect } from 'chai';
-import { filterByTag, getRecipeByName } from '../src/recipes';
+import {
+  filterByTag,
+  getRecipeByName,
+  getIngredientNames,
+  getIngredientPriceSum,
+  getRecipeInstructions,
+} from '../src/recipes';
 
 describe('filterByTag function ', () => {
-  it('Should return a filtered list based on the tag', () => {
-    const recipeData = 
-    [
+  let testIngredientData;
+  let testRecipe;
+  beforeEach(function () {
+    testIngredientData = [
       {
-        "id": 595736,
-        "image": "https://spoonacular.com/recipeImages/595736-556x370.jpg",
-        "ingredients": "",
-        "name": "Dirty Steve's Original Wing Sauce",
-        "tags": [
-          "sauce"
-        ]
+        id: 20081,
+        name: 'wheat flour',
+        estimatedCostInCents: 142,
       },
       {
-        "id": 741603,
-        "image": "https://spoonacular.com/recipeImages/741603-556x370.jpeg",
-        "ingredients": "",
-        "name": "Elvis Pancakes",
-        "tags": [
-          "side dish"
-        ]
-      }
-    ]
-    
-    const result = filterByTag(recipeData, "sauce");
+        id: 18372,
+        name: 'bicarbonate of soda',
+        estimatedCostInCents: 582,
+      },
+      {
+        id: 1123,
+        name: 'eggs',
+        estimatedCostInCents: 472,
+      },
+    ];
 
-    expect(result).to.deep.equal(      
-      [{
-      "id": 595736,
-      "image": "https://spoonacular.com/recipeImages/595736-556x370.jpg",
-      "ingredients": "",
-      "name": "Dirty Steve's Original Wing Sauce",
-      "tags": [
-        "sauce"
-      ]
-    }])
+    testRecipe = {
+      id: 595736,
+      image: 'https://spoonacular.com/recipeImages/595736-556x370.jpg',
+      ingredients: [
+        {
+          id: 20081,
+          quantity: {
+            amount: 1.5,
+            unit: 'c',
+          },
+        },
+        {
+          id: 18372,
+          quantity: {
+            amount: 0.5,
+            unit: 'tsp',
+          },
+        },
+        {
+          id: 1123,
+          quantity: {
+            amount: 1,
+            unit: 'large',
+          },
+        },
+      ],
+      instructions: [
+        {
+          instruction:
+            'In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy.',
+          number: 1,
+        },
+        {
+          instruction: 'Add egg and vanilla and mix until combined.',
+          number: 2,
+        },
+        {
+          instruction:
+            'Add dry ingredients and mix on low just until incorporated. Stir in chocolate chips.Scoop the dough into 1,5 tablespoon size balls and place on a plate or sheet. Cover with saran wrap and chill at least 2 hours or overnight.When ready to bake, preheat oven to 350 degrees.',
+          number: 3,
+        },
+      ],
+      name: 'Loaded Chocolate Chip Pudding Cookie Cups',
+      tags: [
+        'antipasti',
+        'starter',
+        'snack',
+        'appetizer',
+        'antipasto',
+        "hor d'oeuvre",
+      ],
+    };
   });
-})
 
-describe('getRecipeByName function ', () => {
-  it("should return a recipe based on given name", () => {
-    const recipeData = 
-    [
+  it('Should return a filtered list based on the tag', () => {
+    const recipeData = [
       {
-        "id": 595736,
-        "image": "https://spoonacular.com/recipeImages/595736-556x370.jpg",
-        "ingredients": "",
-        "name": "Dirty Steve's Original Wing Sauce",
-        "tags": [
-          "sauce"
-        ]
+        id: 595736,
+        image: 'https://spoonacular.com/recipeImages/595736-556x370.jpg',
+        ingredients: '',
+        name: "Dirty Steve's Original Wing Sauce",
+        tags: ['sauce'],
       },
       {
-        "id": 741603,
-        "image": "https://spoonacular.com/recipeImages/741603-556x370.jpeg",
-        "ingredients": "",
-        "name": "Elvis Pancakes",
-        "tags": [
-          "side dish"
-        ]
-      }
-    ]
+        id: 741603,
+        image: 'https://spoonacular.com/recipeImages/741603-556x370.jpeg',
+        ingredients: '',
+        name: 'Elvis Pancakes',
+        tags: ['side dish'],
+      },
+    ];
 
-    const result = getRecipeByName(recipeData, "elvis pancakes");
+    const result = filterByTag(recipeData, 'sauce');
 
-    expect(result).to.deep.equal({
-      "id": 741603,
-      "image": "https://spoonacular.com/recipeImages/741603-556x370.jpeg",
-      "ingredients": "",
-      "name": "Elvis Pancakes",
-      "tags": [
-        "side dish"
-      ]
-    })
-  })
-
-  it("should not matter if name is capitalized or lowercase", () => {
-    const recipeData = 
-    [
+    expect(result).to.deep.equal([
       {
-        "id": 595736,
-        "image": "https://spoonacular.com/recipeImages/595736-556x370.jpg",
-        "ingredients": "",
-        "name": "Dirty Steve's Original Wing Sauce",
-        "tags": [
-          "sauce"
-        ]
+        id: 595736,
+        image: 'https://spoonacular.com/recipeImages/595736-556x370.jpg',
+        ingredients: '',
+        name: "Dirty Steve's Original Wing Sauce",
+        tags: ['sauce'],
+      },
+    ]);
+  });
+
+  it('should return a recipe based on given name', () => {
+    const recipeData = [
+      {
+        id: 595736,
+        image: 'https://spoonacular.com/recipeImages/595736-556x370.jpg',
+        ingredients: '',
+        name: "Dirty Steve's Original Wing Sauce",
+        tags: ['sauce'],
       },
       {
-        "id": 741603,
-        "image": "https://spoonacular.com/recipeImages/741603-556x370.jpeg",
-        "ingredients": "",
-        "name": "Elvis Pancakes",
-        "tags": [
-          "side dish"
-        ]
-      }
-    ]
+        id: 741603,
+        image: 'https://spoonacular.com/recipeImages/741603-556x370.jpeg',
+        ingredients: '',
+        name: 'Elvis Pancakes',
+        tags: ['side dish'],
+      },
+    ];
 
-    const result = getRecipeByName(recipeData, "ELVIS pAncAkes");
+    const result = getRecipeByName(recipeData, 'elvis pancakes');
 
     expect(result).to.deep.equal({
-      "id": 741603,
-      "image": "https://spoonacular.com/recipeImages/741603-556x370.jpeg",
-      "ingredients": "",
-      "name": "Elvis Pancakes",
-      "tags": [
-        "side dish"
-      ]
-    })
-  })
-})
+      id: 741603,
+      image: 'https://spoonacular.com/recipeImages/741603-556x370.jpeg',
+      ingredients: '',
+      name: 'Elvis Pancakes',
+      tags: ['side dish'],
+    });
+  });
 
+  it('should not matter if name is capitalized or lowercase', () => {
+    const recipeData = [
+      {
+        id: 595736,
+        image: 'https://spoonacular.com/recipeImages/595736-556x370.jpg',
+        ingredients: '',
+        name: "Dirty Steve's Original Wing Sauce",
+        tags: ['sauce'],
+      },
+      {
+        id: 741603,
+        image: 'https://spoonacular.com/recipeImages/741603-556x370.jpeg',
+        ingredients: '',
+        name: 'Elvis Pancakes',
+        tags: ['side dish'],
+      },
+    ];
 
+    const result = getRecipeByName(recipeData, 'ELVIS pAncAkes');
+
+    expect(result).to.deep.equal({
+      id: 741603,
+      image: 'https://spoonacular.com/recipeImages/741603-556x370.jpeg',
+      ingredients: '',
+      name: 'Elvis Pancakes',
+      tags: ['side dish'],
+    });
+  });
+
+  it('getIngredientNames Should be a function', () => {
+    expect(getIngredientNames).to.be.a('function');
+  });
+
+  it('getIngredientPriceSum Should be a function', () => {
+    expect(getIngredientPriceSum).to.be.a('function');
+  });
+
+  it('getRecipeInstructions Should be a function', () => {
+    expect(getRecipeInstructions).to.be.a('function');
+  });
+
+  it('Should return an error message if any function is given a undefined recipe', () => {
+    let test1 = getIngredientNames(undefined);
+    let test2 = getIngredientPriceSum(undefined);
+    let test3 = getRecipeInstructions(undefined);
+    expect(test1).to.equal('Not a valid recipe');
+    expect(test2).to.equal('Not a valid recipe');
+    expect(test3).to.equal('Not a valid recipe');
+  });
+
+  it('Should be able to get Names of Ingredients for a valid recipe name', () => {
+    const recipeName = 'Loaded Chocolate Chip Pudding Cookie Cups';
+    const expectedIngredients = ['wheat flour', 'bicarbonate of soda', 'eggs'];
+    const result = getIngredientNames(testRecipe, testIngredientData);
+    expect(result).to.deep.equal(expectedIngredients);
+  });
+
+  it('Should be able to get the Sum of the prices', () => {
+    let finalSum = getIngredientPriceSum(testRecipe, testIngredientData);
+    expect(finalSum).to.equal(976);
+  });
+
+  it('Should be able to get the directions of the recipe', () => {
+    let instructionsArray = getRecipeInstructions(testRecipe);
+    let finalInstructions = testRecipe.instructions;
+    expect(instructionsArray).to.deep.equal(finalInstructions);
+  });
+});
