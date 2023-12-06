1b98eb1e4b49d66a5390a4891ce63951a9904aac
Cam Johnson <cameron.n.johnson15@gmail.com>
2023-12-06 14:22:20 -0700

    feature: Add dom update features
    Co-authored-by:  Ricky Tran : <therichardtran@gmail.com>
    Co-authored-by:  Jeremy Pearson : <jeremy.pearson08@gmail.com>
    Co-authored-by:  Matthew Shindel : <matthew@shindel.com>

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,16 +1,157 @@
-//NOTE: Your DOM manipulation will occur in this file
-// 
-// Here is an example function just to demonstrate one way you can export/import between the two js files. You'll want to delete this once you get your own code going.
-const displayRecipes = () => {
-  console.log(`Displaying recipes now`)
+import {
+  filterByTag,
+  getRecipeByName,
+  getIngredientNames,
+  getIngredientPriceSum,
+  getRecipeInstructions,
+} from './recipes.js';
+
+import recipeData from './data/recipes.js';
+import ingredientsData from './data/ingredients.js';
+
+// Query Selectors
+const allTags = document.querySelectorAll('.tag-button');
+const displayedRecipesSection = document.querySelector('.recipes-container');
+const tagBar = document.querySelector('.tagbar');
+const topNav = document.querySelector('.topnav');
+const header = document.querySelector('header');
+const fullPageRecipe = document.querySelector('.recipe');
+const recipeTitle = document.querySelector('.recipe-title-h1');
+const tagsParagraph = document.querySelector('.tags');
+const recipeImage = document.querySelector('.recipe-picture');
+const priceParagraph = document.querySelector('.price-text');
+const recipeIngredientsSection = document.querySelector('.recipe-ingredients');
+const recipeInstructionsSection = document.querySelector('.recipe-instructions');
+const xBtn = document.querySelector('.x-button');
+const searchInput = document.querySelector('.search-input');
+
+// Event Listeners
+allTags.forEach((tag) => {
+  tag.addEventListener('click', returnListByTag);
+});
+
+searchInput.addEventListener('keypress', returnSearchedRecipe);
+
+window.addEventListener('load', generateRecipes(recipeData));
+
+header.addEventListener('click', function(event) {
+  if (event.target.classList.contains('x-button')) {
+    header.style.backgroundImage = "url('images/whats-cooking-banner.jpg')";
+    header.style.backgroundColor = '';
+    header.style.height = '250px';
+    xBtn.classList.add('hidden');
+    topNav.classList.remove('hidden');
+    tagBar.classList.remove('hidden');
+    displayedRecipesSection.classList.remove('hidden');
+    fullPageRecipe.classList.add('hidden');
+  }
+})
+
+displayedRecipesSection.addEventListener('click', function (event) {
+  displayRecipe(event);
+});
+
+
+// Functions
+function returnSearchedRecipe(event) {
+  if (event.key === 'Enter') {
+    const searchText = event.target.value;
+    const result = (getRecipeByName(recipeData, searchText));
+
+    if (result) {
+      generateRecipes(result);
+    }
+  }
 }
 
-function testFunction2 () {
-  console.log("hi");
+function returnListByTag(event){
+  const buttonID = event.target.id;
+
+  if (buttonID === 'all'){
+    const filteredRecipes = recipeData;
+    generateRecipes(filteredRecipes);
+  } else {
+    const filteredRecipes =  filterByTag(recipeData, buttonID);
+    generateRecipes(filteredRecipes);
+  }
 }
 
+function generateRecipes(recipes) {
+  displayedRecipesSection.innerHTML = '';
 
-export {
-  displayRecipes,
-  testFunction2,
+  recipes.forEach((recipe) => {
+    displayedRecipesSection.innerHTML += `
+    <div class="recipe-item">
+      <img class="recipe-image" src="${recipe.image}" alt="Recipe Photo">
+      <p>${recipe.name}</p>
+    </div>
+    `;
+  });
 }
+
+function displayRecipe(event) {
+  if (event.target.closest('div').classList.contains('recipe-item')) {
+    header.style.backgroundImage = 'none';
+    header.style.backgroundColor = '#e7e5e6';
+    header.style.height = '70px';
+    show(xBtn);
+    show(fullPageRecipe);
+    hide(topNav);
+    hide(tagBar);
+    hide(displayedRecipesSection);
+
+    const recipe = recipeData.find(
+      (recipe) =>
+        recipe.name ===
+        event.target.closest('div').querySelector('p').textContent
+    );
+
+    const price = getIngredientPriceSum(recipe, ingredientsData) / 100;
+
+    generateRecipeTitle(recipe.name);
+    generateRecipeTags(recipe.tags);
+    generateRecipeImage(recipe.image);
+    generateRecipePrice(price);
+    generateRecipeIngredients(recipe.ingredients);
+    generateRecipeInstructions(recipe.instructions);
+  }
+}
+
+function generateRecipeTitle(title) {
+  recipeTitle.innerText = title;
+}
+
+function generateRecipeTags(tags) {
+  tagsParagraph.innerHTML = `<span class="orange-text">Tags:</span> ${tags.join(
+    ', '
+  )}`;
+}
+
+function generateRecipeImage(image) {
+  recipeImage.innerHTML = `<img src="${image}" alt="Recipe Image">`;
+}
+
+function generateRecipePrice(price) {
+  recipeIngredientsSection.innerHTML = '';
+  recipeIngredientsSection.innerHTML = `<h3>Recipe Cost</h3><p class="price-text">$${price.toFixed(2)}</p>`
+}
+
+function generateRecipeIngredients(ingredients) {
+  recipeIngredientsSection.innerHTML += '<h3>Ingredients</h3>'
+  ingredients.forEach(ingredient => recipeIngredientsSection.innerHTML += `<p>${ingredient.quantity.amount} ${ingredient.quantity.unit} ${ingredientsData.find(iteration => iteration.id === ingredient.id).name}</p>`)
+}
+
+function generateRecipeInstructions(instructions) {
+  recipeInstructionsSection.innerHTML = ''
+  instructions.forEach(instruction => recipeInstructionsSection.innerHTML += `<h3>Step ${instruction.number}</h3><p>${instruction.instruction}</p>`)
+}
+
+function hide(element) {
+  element.classList.add('hidden');
+}
+
+function show(element) {
+  element.classList.remove('hidden');
+}
+
+export { returnListByTag, generateRecipes, returnSearchedRecipe };
