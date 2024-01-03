9eb1f41325bd684422c592710aa917e44e664120
MatthewShindel <matthew@shindel.com>
2024-01-03 15:31:39 -0500

    cleaned apiCall.js functions

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -5,30 +5,18 @@ import { randomizeUser } from "./users";
 
 export function getApiInfo(type) {
 	//only use users, ingredients, recipes for type
-	let apiURL = ""
+	let apiURL = "https://what-s-cookin-starter-kit.herokuapp.com/api/v1/"
+	apiURL += type;
 	let apiResponse;
-	switch (type) {
-		case 'users':
-			apiURL = 'https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users'
-			apiResponse = fetch(apiURL)
-			.then(response => response.json()
-			.then(data => randomizeUser(data.users)))
-			break;
-		case 'recipes':
-			apiURL = 'https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes'
-			apiResponse = fetch(apiURL)
-			.then(response => response.json()
-			.then());
-			break;
-		case 'ingredients':
-			apiURL = 'https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients'
-			apiResponse = fetch(apiURL)
-			.then(response => response.json()
-			.then());
-			break;
-		default:
-			console.log(`ERROR: Could not match case: ${type}`);
-			break;
+	if (type === 'users') {
+		apiResponse = fetch(apiURL)
+		.then(response => response.json()
+		.then(data => randomizeUser(data.users)))
+		return apiResponse;
+	} else {
+		apiResponse = fetch(apiURL)
+		.then(response => response.json()
+		.then());
+		return apiResponse;
 	}
-	return apiResponse;
 }
