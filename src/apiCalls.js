9150451a215a455c81ee0bb20bfd0886a9cb4b2c
MatthewShindel <matthew@shindel.com>
2024-01-03 15:43:58 -0500

    cleaned some comments in code

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,8 +1,5 @@
 import { randomizeUser } from "./users";
 
-// Your fetch requests will live here!
-
-
 export function getApiInfo(type) {
 	//only use users, ingredients, recipes for type
 	let apiURL = "https://what-s-cookin-starter-kit.herokuapp.com/api/v1/"
